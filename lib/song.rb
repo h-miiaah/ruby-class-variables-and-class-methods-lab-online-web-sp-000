30ac10a10eaa9fd5e1b02ec230224db8f1f94fd7
Haim D <haim11293@aol.com>
2019-09-18 12:22:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,14 +23,20 @@ class Song
     @@genres.uniq
   end
 
-  def self.artists
-    @@artists.uniq
-  end
-
   def self.genre_count
     genre_count = {}
     @@genres.each {|genre| genre_count[genre] += 1}
     return genre_count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each {|artist| artist_count[artist] += 1}
+    return artist_count
   end
 
 end
