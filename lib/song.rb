2584a7c08fc5317f692e67886db314c3b1451979
Haim D <haim11293@aol.com>
2019-09-18 12:44:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,13 @@ class Song
 
   def self.genre_count
     genre_count = {} # sets genre_count to an empty hash.
-    @@genres.each {|genre| genre_count[genre] += 1} # iterate through each genre in the @@genres array and increment it by 1.
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
     genre_count # retruns the genre_count hash.
   end
 
